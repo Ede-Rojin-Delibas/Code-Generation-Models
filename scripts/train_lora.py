9cb655d9a89767c521f1691f9422c902c2180e0f
Ede Rojin DELİBAŞ <ederojind@gmail.com>
2026-01-04 12:39:31 +0300

    syntax and training errors have dealed.

--- scripts/train_lora.py
+++ scripts/train_lora.py
@@ -6,6 +6,8 @@ from transformers import (AutoModelForCausalLM, #It's for code generation model
                             Trainer, #training loop
                             DataCollatorForLanguageModeling) #bathching
 from peft import LoraConfig, get_peft_model #for adding LoRA adapters
+from transformers import default_data_collator
+
 
 MODEL_NAME="TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 DATASET_PATH="data/processed/test_small.jsonl"
@@ -44,28 +46,30 @@ tokenized_dataset=dataset.map(
 
 model=AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
-    dtype=torch.float16,
+    torch_dtype=torch.float16,
     device_map="auto"
 )
-
+model.gradient_checkpointing_enable()
+model.enable_input_require_grads()
 lora_config=LoraConfig(
     r=8,
     lora_alpha=16,
-    target_modules=["q_proj","v_proj"],
+    target_modules=["q_proj","k_proj","v_proj","o_proj"],
     lora_dropout=0.05,
     bias="none",
     task_type="CAUSAL_LM"
 )
 
 model=get_peft_model(model,lora_config)
-model.gradient_checkpointing_enable()
 model.print_trainable_parameters()
 
 training_args=TrainingArguments(
     output_dir=OUTPUT_DIR,
     per_device_train_batch_size=BATCH_SIZE,
+    gradient_accumulation_steps=4,
     num_train_epochs=EPOCHS,
     learning_rate=LR,
+    warmup_steps=2,
     logging_steps=1,
     save_strategy="epoch",
     fp16=True,
@@ -76,10 +80,7 @@ trainer=Trainer(
     model=model,
     args=training_args,
     train_dataset=tokenized_dataset,
-    data_collator=DataCollatorForLanguageModeling(
-        tokenizer=tokenizer,
-        mlm=False
-    )
+    data_collator=default_data_collator
 )
 trainer.train()
 
